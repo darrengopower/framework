dc415fe8fbb3665193d4f4184bd18cb5b60a0992
twilroad <269044570@qq.com>
2015-11-27 17:34:23 +0800

    调整目录结构

--- src/Notadd/Foundation/Application.php
+++ src/Notadd/Foundation/Application.php
@@ -25,7 +25,7 @@ class Application extends Container implements ApplicationContract, HttpKernelIn
     /**
      * @var string
      */
-    const VERSION = '0.1.0';
+    const VERSION = '0.1.1';
     /**
      * @var string
      */
